f9175b4a79b53cf24af576e8468e10b1e24827d8
AintzaneGa <46562547+AintzaneGa@users.noreply.github.com>
2019-01-10 13:34:06 +0100

    Update Autobusa.java

--- Autobusa.java
+++ Autobusa.java
@@ -3,7 +3,9 @@ public class Autobusa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int x = 0;int y = 0;
+		int gehitu=x+y;
+		System.out.println(gehitu);
 	}
 
 }
